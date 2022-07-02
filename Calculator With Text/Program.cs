85ad40a2cb2a0ea2c4b15c0bfea98cd7dc9f196b
meteh <meteh@SAMSUNG-PC>
2022-07-02 23:31:59 +0300

    Proje dosyası ekle.

---
+++ Calculator With Text/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+Console.WriteLine("Type in the location");
+var path = Console.ReadLine();
+string[] readText = File.ReadAllLines(path);
+foreach (string s in readText)
+{
+
+    Console.WriteLine(s);
+
+    string expression = Convert.ToString(s);
+    var result = new DataTable().Compute(expression, "") + "";
+    Console.WriteLine(result);
+}
